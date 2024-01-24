7c7409f1b379791b7f2a04649f5bb807eaa84b8b
csachy13 <80550433+csachy13@users.noreply.github.com>
2024-01-24 18:04:15 -0500

    Create script.js

---
+++ unclock/script.js
@@ -0,0 +1,15 @@
+var timeText = document.getElementById("time");
+var dateText = document.getElementById("date");
+const updateTime = () => {
+    let time = new Date()
+    let hours = (time.getHours()%12).toString()
+    let ampm = Math.floor(time.getHours()/12) === 2 ? "AM" : "PM"
+    let minutes = time.getMinutes().toString()
+    let seconds = time.getSeconds().toString()
+    timeText.innerHTML = `${('0'+hours).slice(-2)}<sub>${ampm}</sub>:${('0'+minutes).slice(-2)}:${('0'+seconds).slice(-2)}`
+    dateText.innerHTML = `${dayNames[time.getDay()]}, ${month[time.getMonth()]} ${time.getDate()}<br><year>${time.getFullYear()}</year>`
+}
+var month= ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+updateTime()
+setInterval(updateTime, 1000)
